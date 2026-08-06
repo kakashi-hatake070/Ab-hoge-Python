bc2243a1fca0948bbc93ba015525c9a3cb142f9a
kunal sharma <sharmakunal7184@gmail.com>
2026-08-06 18:42:04 +0530

    Initial commit: Ab hoge Python

---
+++ first.py
@@ -0,0 +1,9 @@
+# wap to all the digit present inside the given number.
+a = int(input("enter a number"))
+sum=0
+while a!=0:
+    ld=%2==0:
+    if ld%2==0:
+    sum=sum+ld
+num=num//10
+print(sum)
